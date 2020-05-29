095b29052fd3b33bc6b22453b8ca7d259778cdc3
Ricardo Nagel Rodrigues <ricardonagel@gmail.com>
2020-05-29 00:42:19 -0300

    Organizado imports

--- service/totocr/ocr/Totocr.py
+++ service/totocr/ocr/Totocr.py
@@ -3,9 +3,10 @@ from lxml import etree, html
 import numpy as np
 import ocr.hocr_tools
 from PIL import Image
+import pytesseract
 
 def OcrTesseract(image,lang='por+eng',config='--psm 1 --oem 1'):
-    import pytesseract
+
 
     #Chamada Tesseract
     hocr_bytes = pytesseract.image_to_pdf_or_hocr(image, lang=lang, extension='hocr', config=config)
